b6974e7cd7fee28ae45229dff9421968abbae8f5
Fakhri Rahmat Rizqi <fakhrihzqi@gmail.com>
2024-01-01 23:54:55 +0700

    first and completed commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Qwords.com: Cloud Web Hosting Indonesia Akses Cepat Aman</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header container">
+        <div class="discount">
+            <h5 class="discount-alert">🎁 DISKON AKHIR TAHUN, POTONGAN BIAYA HOSTING HINGGA <span>50%</span></h5>
+        </div>
+        <nav class="navbar">
+            <div class="header-logo">
+                <img src="logo-qwods" alt="Qwords Logo">
+            </div>
+            
+            <ul class="header-menu">
+                <li>
+                    <a class="header-link" href="#">Cloud Hosting</a>
+                </li>
+                <li>
+                    <a class="header-link" href="#">Server</a>
+                </li>
+                <li>
+                    <a class="header-link" href="#">Domain</a>
+                </li>
+                <li>
+                    <a class="header-link" href="#">Email Suite</a>
+                </li>
+                <li>
+                    <a class="header-link" href="#">Services</a>
+                </li>
+                <li>
+                    <a class="header-link" href="#">Internet Access</a>
+                </li>
+                <li>
+                    <a class="header-link" href="#">Customer Care</a>
+                </li>
+            </ul>
+
+            <div class="cta">
+                <button class="nav-btn">Daftar</button>
+                <button class="nav-btn">Log in</button>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="hero">
+            <div class="hero-left">
+                <h1 class="hero-title">
+                    Cloud Hosting Indonesia untuk Kesuksesan Websitemu!
+                </h1>
+                <h3 class="hero-description">
+                    Qwords menawarkan layanan Cloud Hosting Akses Cepat, Standar Keamanan Tinggi, Uptime Server 99,99%, Fitur Lengkap dan Dukungan Pelanggan 24 jam untuk memenuhi kebutuhan website Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3 dan Network Tier 1 
+                </h3>
+                <button class="hero-cta">Cek Domain Dulu</button>
+            </div>
+           <div class="hero-right">
+            <img class="hero-img" src="hero-img.svg" alt="heroimg">
+           </div>
+        </section>
+        <section class="domain">
+            <h1 class="domain-title">Cari Nama Domainmu</h1>
+            <div class="input-domain">
+                <input class="input" type="text" placeholder="Cari nama domain Anda">
+                <button class="domain-btn">Cari</button>
+            </div>
+            <div class="domain-content">
+                <div class="domain-item">
+                    <h2>.com</h2>
+                    <h3>Rp 165.000</h3>
+                </div>
+                <div class="domain-item">
+                    <h2>.id</h2>
+                    <h3>Rp 225.000</h3>
+                </div>
+                <div class="domain-item">
+                    <h2>.org</h2>
+                    <h3>Rp 130.000</h3>
+                </div>
+            </div>
+            
+        </section>
+        <section class="bantuan">
+            <div class="bantuan-title">
+                <h1>Contact Us</h1>
+                <h2>24 Jam Layanan</h2>
+            </div>
+    
+            <ul class="bantuan-menu">
+                <li class="bantuan-item">
+                    <a href="#">0804-1-808-888</a>
+                </li>
+                <li class="bantuan-item">
+                    <a href="#">021 39708800</a>
+                </li>
+                <li class="bantuan-item">
+                    <a href="#">WhatsApp</a>
+                </li>
+                <li class="bantuan-item">
+                   <a href="#">info@qwords.com</a>
+                </li>
+            </ul>
+        </section>
+    </main>
+
+    <footer class="footer-section">
+
+    </footer>
+</body>
+</html>
